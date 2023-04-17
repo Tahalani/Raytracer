694cb7ae55abf38cd690b4ca65b62280af2580e0
Taha ALANI <taha.alani@epitech.eu>
2023-04-17 14:59:22 +0200

    feat: help

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+fn print_help(binary: &String) {
+    println!("Usage: {} <SCENE_FILE>", binary);
+    println!("\tSCENE_FILE: scene configuration");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args[1].eq("--help") {
+        print_help(&args[0]);
+    }
+    // dbg!(args);
 }
