fb242055aec21122e737c00d9d4362a3e2394153
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-05-01 16:41:35 +0200

    feat(ray): display sphere in plan

--- src/camera.rs
+++ src/camera.rs
@@ -5,3 +5,25 @@
 // camera
 //
 
+use crate::rectangle::Rectangle3D;
+use crate::point::Point3D;
+use crate::ray::Ray;
+
+#[derive(Clone, Copy, Debug)]
+
+pub struct Camera {
+    pub origin: Point3D,
+    pub screen: Rectangle3D,
+}
+
+impl Camera {
+    pub fn init_camera(origin: Point3D, screen: Rectangle3D) -> Camera {
+        Camera { origin, screen }
+    }
+
+    pub fn ray(&self, u: f64, v: f64) -> Ray {
+        let screen_point = self.screen.pointAt(u, v);
+        let direction = screen_point - self.origin;
+        return Ray::init_ray(self.origin, direction);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod point;
 mod ray;
 mod sphere;
 mod rectangle;
+mod camera;
 
 // use vector::{Vector};
 use std::process::exit;
@@ -20,6 +21,28 @@ fn print_help(binary: &String) {
     println!("\tSCENE_FILE: scene configuration");
 }
 
+fn display_shere(_camera: camera::Camera)
+{
+    let _pointSphere = point::Point3D::init_point(0.5, 0.5, 1.0);
+    let sphere = sphere::Sphere::init_sphere(_pointSphere, 0.1);
+
+    for y in (0..=100).rev() {
+        for x in 0..=100 {
+            let _ray = _camera.ray(x as f64 / 100.0, y as f64 / 100.0);
+            let _hits = sphere.hits(_ray);
+            if _hits {
+                print!("O");
+            } else {
+                print!(" ");
+            }
+        }
+        print!("\n");
+    }
+    // println!("Ray: {:?}", _ray2);
+
+
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if  args.len() == 2 && args[1].eq("--help") {
@@ -28,12 +51,17 @@ fn main() {
     }
     let _vector = vector::Vector::init_vector(1.0, 0.0, 0.0);
     let _vector2 = vector::Vector::init_vector(0.0, 1.0, 0.0);
+    let _pointrec = point::Point3D::init_point(0.0, 0.0, 1.0);
     let _point = point::Point3D::init_point(0.0, 0.0, 0.0);
-    let _ray = ray::Ray::init_ray(_point, _vector);
-    let _rectangle = rectangle::Rectangle3D::init_rectangle(_point, _vector, _vector2);
-    let _point2 = _rectangle.pointAt(1.0, 0.0);
+    // let _ray = ray::Ray::init_ray(_point, _vector);
+    let _rectangle =
+    rectangle::Rectangle3D::init_rectangle(_pointrec, _vector, _vector2);
 
-    println!("Point: {:?}", _point2);
+    let _camera: camera::Camera = camera::Camera::init_camera(_point, _rectangle);
+
+    display_shere(_camera);
+
+    // println!("Point: {:?}", _point2);
     // let _sphere = sphere::Sphere::init_sphere(_point, 10.0);
     // let _hits = _sphere.hits(_ray);
     // println!("Hits: {}", _hits);
