26dcecc0a69c5185758f1e92899fdf6b93714a1b
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-05-01 17:47:20 +0200

    fix(main): clean code in function

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,8 @@ fn print_help(binary: &String) {
     println!("\tSCENE_FILE: scene configuration");
 }
 
-fn display_shere(_camera: camera::Camera)
+fn display_sphere(_camera: camera::Camera, sphere: sphere::Sphere)
 {
-    let _pointSphere = point::Point3D::init_point(0.5, 0.5, 1.0);
-    let sphere = sphere::Sphere::init_sphere(_pointSphere, 0.1);
-
     for y in (0..=100).rev() {
         for x in 0..=100 {
             let _ray = _camera.ray(x as f64 / 100.0, y as f64 / 100.0);
@@ -36,11 +33,24 @@ fn display_shere(_camera: camera::Camera)
                 print!(" ");
             }
         }
-        print!("\n");
+        println!("");
     }
-    // println!("Ray: {:?}", _ray2);
+}
 
+fn make_screen() {
 
+    let bottom_side = vector::Vector::init_vector(1.0, 0.0, 0.0);
+    let left_side = vector::Vector::init_vector(0.0, 1.0, 0.0);
+    let origin_rectangle = point::Point3D::init_point(0.0, 0.0, 1.0);
+    let _rectangle = rectangle::Rectangle3D::init_rectangle(origin_rectangle, bottom_side, left_side);
+
+    let origin_cam = point::Point3D::init_point(0.0, 0.0, 0.0);
+    let _camera: camera::Camera = camera::Camera::init_camera(origin_cam, _rectangle);
+
+    let point_sphere = point::Point3D::init_point(0.5, 0.5, 1.0);
+    let sphere = sphere::Sphere::init_sphere(point_sphere, 0.1);
+
+    display_sphere(_camera, sphere);
 }
 
 fn main() {
@@ -48,30 +58,8 @@ fn main() {
     if  args.len() == 2 && args[1].eq("--help") {
         print_help(&args[0]);
         exit(0);
+    } else {
+        make_screen();
     }
-    let _vector = vector::Vector::init_vector(1.0, 0.0, 0.0);
-    let _vector2 = vector::Vector::init_vector(0.0, 1.0, 0.0);
-    let _pointrec = point::Point3D::init_point(0.0, 0.0, 1.0);
-    let _point = point::Point3D::init_point(0.0, 0.0, 0.0);
-    // let _ray = ray::Ray::init_ray(_point, _vector);
-    let _rectangle =
-    rectangle::Rectangle3D::init_rectangle(_pointrec, _vector, _vector2);
 
-    let _camera: camera::Camera = camera::Camera::init_camera(_point, _rectangle);
-
-    display_shere(_camera);
-
-    // println!("Point: {:?}", _point2);
-    // let _sphere = sphere::Sphere::init_sphere(_point, 10.0);
-    // let _hits = _sphere.hits(_ray);
-    // println!("Hits: {}", _hits);
-    // let _sphere = sphere::Sphere::init_sphere(_point, 10.0);
-    // let _vector3 = _vector - _vector2;
-
-    // let _point2 = _point + _vector;
-    // println!("Vector: {:?}", _vector3);
-    // println!("Point: {:?}", _point2);
-    // let _length = _vector.length_vector();
-    // println!("Length: {}", _length);
-    // dbg!(args);
 }
